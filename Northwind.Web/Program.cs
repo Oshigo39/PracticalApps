abb4bbd84bb38b0395841d0298db274a1058a5e6
Oshigo <1950519496@qq.com>
2025-06-21 21:56:09 +0800

    Info: 配置html的默认界面

--- Northwind.Web/Program.cs
+++ Northwind.Web/Program.cs
@@ -11,12 +11,17 @@ if (!app.Environment.IsDevelopment())
 // 添加将 HTTP 请求重定向到 HTTPS 的中间件。
 app.UseHttpsRedirection();
 
+// 在当前 pat 上启用默认文件映射（UseDefaultFiles必须在UseStaticFiles之前）
+app.UseDefaultFiles();
+// 为当前请求路径启用静态文件服务
+app.UseStaticFiles();
+
 // 为所有的get请求都指向Hello World
-app.MapGet("/", () => $"Environment is: {app.Environment.EnvironmentName}");
+app.MapGet("/hello", () => $"Environment is: {app.Environment.EnvironmentName}");
 
 #endregion
 
 // 启动网络服务器，托管网站并等待请求
 // 这是一个线程阻塞调用
 app.Run();
-Console.WriteLine("这是在Web服务器停止后执行的。");
+Console.WriteLine("由于线程阻塞，这是在Web服务器停止后执行的。");
