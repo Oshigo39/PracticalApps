d304d91398120eb0b5a9a773a18c8a55c0ae573c
Oshigo <1950519496@qq.com>
2025-06-22 09:54:24 +0800

    Info: 创建SuppliersModel展示供应商公司名称（硬编码）

---
+++ Northwind.Web/Pages/SuppliersModel.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Northwind.Web.Pages;
+
+public class SuppliersModel : PageModel
+{
+    // 可枚举的Suppliers集合的属性
+    public IEnumerable<string>? Suppliers { get; set; }
+    
+    public void OnGet()
+    {
+        ViewData["Title"] = "Northwind B2B - Suppliers";
+        // 硬编码集合（一个get的http请求生命周期内生效）
+        Suppliers =
+        [
+            "Alpha Co", "Beta Limited", "Gamma Corp"
+        ];
+    }
+}
